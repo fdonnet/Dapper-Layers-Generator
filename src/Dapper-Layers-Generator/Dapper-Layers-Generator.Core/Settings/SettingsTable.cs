40014322ee8f9b8b5ea46d4dc186ed37217b059d
Fabien Donnet <fabien.donnet@ubiko.ch>
2022-11-08 20:46:03 +0100

    Remove one useless settings and clean

--- src/Dapper-Layers-Generator/Dapper-Layers-Generator.Core/Settings/SettingsTable.cs
+++ src/Dapper-Layers-Generator/Dapper-Layers-Generator.Core/Settings/SettingsTable.cs
@@ -14,37 +14,34 @@
         [SettingsAttribute(Message = "Ignored column names for Update operation (separator ,): ", Position = 4, IsColumnListChoice = true)]
         public string IgnoredColumnNamesForUpdate { get; set; } = string.Empty;
 
-        [SettingsAttribute(Message = "Ignored column names for Delete operation (separator ,): ", Position = 5, IsColumnListChoice = true)]
-        public string IgnoredColumnNamesForDelete { get; set; } = string.Empty;
-
-        [SettingsAttribute(Message = "JSON ignore decoration for column names  (separator ,): ", Position = 6, IsColumnListChoice = true)]
+        [SettingsAttribute(Message = "JSON ignore decoration for column names  (separator ,): ", Position = 5, IsColumnListChoice = true)]
         public string JsonIgnoreDecoration { get; set; } = string.Empty;
 
-        [SettingsAttribute(Message = "Enable get all function generator: ", Position = 7, Group = "Generator")]
+        [SettingsAttribute(Message = "Enable get all function generator: ", Position = 6, Group = "Generator")]
         public bool GetAllGenerator { get; set; } = true;
 
-        [SettingsAttribute(Message = "Enable get by primary key function generator: ", Position = 8, Group = "Generator")]
+        [SettingsAttribute(Message = "Enable get by primary key function generator: ", Position = 7, Group = "Generator")]
         public bool GetByPkGenerator { get; set; } = true;
 
-        [SettingsAttribute(Message = "Enable get by primary keys list function generator: ", Position = 9, Group = "Generator")]
+        [SettingsAttribute(Message = "Enable get by primary keys list function generator: ", Position = 8, Group = "Generator")]
         public bool GetByPkListGenerator { get; set; } = true;
 
-        [SettingsAttribute(Message = "Enable get by unique key function generator: ", Position = 10, Group = "Generator")]
+        [SettingsAttribute(Message = "Enable get by unique key function generator: ", Position = 9, Group = "Generator")]
         public bool GetByUkGenerator { get; set; } = true;
 
-        [SettingsAttribute(Message = "Enable add function generator: ", Position = 12, Group = "Generator")]
+        [SettingsAttribute(Message = "Enable add function generator: ", Position = 10, Group = "Generator")]
         public bool AddGenerator { get; set; } = true;
 
-        [SettingsAttribute(Message = "Enable update function generator: ", Position = 13, Group = "Generator")]
+        [SettingsAttribute(Message = "Enable update function generator: ", Position = 11, Group = "Generator")]
         public bool UpdateGenerator { get; set; } = true;
 
-        [SettingsAttribute(Message = "Enable delete function generator: ", Position = 14, Group = "Generator")]
+        [SettingsAttribute(Message = "Enable delete function generator: ", Position = 12, Group = "Generator")]
         public bool DeleteGenerator { get; set; } = true;
 
-        [SettingsAttribute(Message = "(NOT IMPLEMENTED) Enable add bulk function generator: ", Position = 15, Group = "Generator")]
+        [SettingsAttribute(Message = "(NOT IMPLEMENTED) Enable add bulk function generator: ", Position = 13, Group = "Generator")]
         public bool AddBulkGenerator { get; set; } = false;
 
-        [SettingsAttribute(Message = "(NOT IMPLEMENTED) Enable update bulk function generator: ", Position = 16, Group = "Generator")]
+        [SettingsAttribute(Message = "(NOT IMPLEMENTED) Enable update bulk function generator: ", Position = 14, Group = "Generator")]
         public bool UpdateBulkGenerator { get; set; } = false;
 
         //Global table settings 
