09942cda2441a2999a2be070b8cb861680815e97
fdonnetTaff <fabien.donnet@ubiko.ch>
2022-12-01 16:14:26 +0100

    poco generator standard test

--- test/Dapper-Layers-Generator.Test/Generators/GeneratorPOCO_Tests.cs
+++ test/Dapper-Layers-Generator.Test/Generators/GeneratorPOCO_Tests.cs
@@ -12,20 +12,27 @@ using Dapper_Layers_Generator.Core.Converters.MySql;
 using Moq;
 using System.Text.Json;
 using Dapper_Layers_Generator.Data.POCO;
+using Dapper_Layers_Generator.Core.Settings;
 
 namespace Dapper_Layers_Generator.Core.Generators.Tests
 {
     public class GeneratorPOCO_Tests : GeneratorBaseTest
     {
+        //need to be reviewed if dataconverted will change between db provider
+        private MySqlDataTypeConverter _dataConverter;
+        private StringTransformationService _transformString;
+
+        public GeneratorPOCO_Tests() : base() 
+        {
+            _dataConverter = new MySqlDataTypeConverter();
+            _transformString = new StringTransformationService(_settings);
+        }
 
         [Fact()]
         public void SetTableNotFound_Test()
         {
             //Arrange
-            var transformString = new StringTransformationService(_settings);
-            //need to be reviewed if dataconverted will change between db provider
-            var dataConverter = new MySqlDataTypeConverter();
-            var generator = new GeneratorPOCO(_settings, _mockDbDefinitions.Object, transformString, dataConverter);
+            var generator = new GeneratorPOCO(_settings, _mockDbDefinitions.Object, _transformString, _dataConverter);
 
             //Act
             var exception = Record.Exception(() => generator.SetTable("zzzz"));
@@ -40,16 +47,13 @@ namespace Dapper_Layers_Generator.Core.Generators.Tests
         public void SetTableColumnsNotFound_Test()
         {
             //Arrange
-            var transformString = new StringTransformationService(_settings);
-            //need to be reviewed if dataconverted will change between db provider
-            var dataConverter = new MySqlDataTypeConverter();
             var jsonDB = ResourceTool.Read("Dapper_Layers_Generator.Test/Data/schema.json");
             var schemas = JsonSerializer.Deserialize<List<Schema>>(jsonDB) ?? throw new NullReferenceException("Cannot test without JSON DB");
             schemas[0].Tables!.Where(t => t.Name == "clients").Single().Columns =null;
             var mockDbDefinitions = new Mock<IReaderDBDefinitionService>();
             mockDbDefinitions.Setup(x => x.SchemaDefinitions).Returns(schemas);
 
-            var generator = new GeneratorPOCO(_settings, mockDbDefinitions.Object, transformString, dataConverter);
+            var generator = new GeneratorPOCO(_settings, mockDbDefinitions.Object, _transformString, _dataConverter);
 
             //Act
             var exception = Record.Exception(() => generator.SetTable("clients"));
@@ -60,9 +64,18 @@ namespace Dapper_Layers_Generator.Core.Generators.Tests
         }
 
         [Fact()]
-        public void Generate_Test()
+        public void GenerateStandard_Test()
         {
+            //Arrange
+            var generator = new GeneratorPOCO(_settings, _mockDbDefinitions.Object, _transformString, _dataConverter);
+            generator.SetTable("clients");
+            var expected = ResourceTool.Read("Dapper_Layers_Generator.Test/Results/Generators/GeneratorPOCO_ResultStandard.txt");
 
+            //Act
+            var result = generator.Generate();
+
+            //Assert
+            Assert.Equal(expected, result);
         }
     }
 }
