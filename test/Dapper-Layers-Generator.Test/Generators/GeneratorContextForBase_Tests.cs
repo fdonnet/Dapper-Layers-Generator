fa6829c8363d129f5ac8bca7337e7e2a1b292255
fdonnetTaff <fabien.donnet@ubiko.ch>
2022-12-01 13:42:35 +0100

    DbContextBase testing

--- test/Dapper-Layers-Generator.Test/Generators/GeneratorContextForBase_Tests.cs
+++ test/Dapper-Layers-Generator.Test/Generators/GeneratorContextForBase_Tests.cs
@@ -41,5 +41,24 @@ namespace Dapper_Layers_Generator.Core.Generators.Tests
             Assert.Equal(expected, result);
         }
 
+        [Fact()]
+        public void GenerateChangeDbContextName_Test()
+        {
+            //Arrange
+            var transformString = new StringTransformationService(_settings);
+            var generator = new GeneratorContextForBase(_settings, _mockDbDefinitions.Object, transformString);
+            _settings.DbContextClassName = "TestOtherDBContext";
+
+            var expected = ResourceTool.Read("Dapper_Layers_Generator.Test/Results/Generators/GeneratorContextForBase_ResultOtherDbContextName.txt");
+
+            //Act
+            var result = generator.Generate();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+
+
     }
 }
