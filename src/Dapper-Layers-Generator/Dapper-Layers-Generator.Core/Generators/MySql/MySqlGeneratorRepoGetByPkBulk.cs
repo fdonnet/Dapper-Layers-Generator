7da9bc99f74bef63f2c80d461cd3200b351564c9
Fabien Donnet <fabien.donnet@ubiko.ch>
2022-12-02 14:21:48 +0100

    c# 11 literal for pkbulk generator

--- src/Dapper-Layers-Generator/Dapper-Layers-Generator.Core/Generators/MySql/MySqlGeneratorRepoGetByPkBulk.cs
+++ src/Dapper-Layers-Generator/Dapper-Layers-Generator.Core/Generators/MySql/MySqlGeneratorRepoGetByPkBulk.cs
@@ -22,76 +22,55 @@ namespace Dapper_Layers_Generator.Core.Generators.MySql
 
         public override string Generate()
         {
-            if (TableSettings.DeleteBulkGenerator && !string.IsNullOrEmpty(GetPkMemberNamesString()))
+            if (TableSettings.GetByPkBulkGenerator && !string.IsNullOrEmpty(GetPkMemberNamesString()))
             {
-                var output = new StringBuilder();
-                output.Append(WriteMethodDef());
-                output.Append(Environment.NewLine);
-                output.Append(WriteOpenTransAndInitBulkMySql());
-                output.Append(Environment.NewLine);
-                output.Append(Environment.NewLine);
-                output.Append(WriteCreateDbTmpTableForPksMySql("get"));
-                output.Append(Environment.NewLine);
-                output.Append(Environment.NewLine);
-                output.Append(WriteCreateDataTableForPkMySql("get"));
-                output.Append(Environment.NewLine);
-                output.Append(Environment.NewLine);
-                output.Append(WriteBulkCallMySql());
-                output.Append(Environment.NewLine);
-                output.Append(Environment.NewLine);
-                output.Append(GetSelectFromTmpTable());
-                output.Append(Environment.NewLine);
-                output.Append(Environment.NewLine);
-                output.Append(WriteDapperCall());
-                output.Append(Environment.NewLine);
-                output.Append(Environment.NewLine);
-                output.Append(WriteCloseTransaction());
-                output.Append(Environment.NewLine);
-                output.Append(Environment.NewLine);
-                output.Append(WriteReturnObj());
-                output.Append(Environment.NewLine);
-                output.Append($"{tab}{tab}}}");
-                output.Append(Environment.NewLine);
-                output.Append(Environment.NewLine);
+                return
+                    $$"""
+                    {{WriteMethodDef()}}
+                    {{WriteOpenTransAndInitBulkMySql()}}
 
-                return output.ToString();
+                    {{WriteCreateDbTmpTableForPksMySql("get")}}
+
+                    {{WriteCreateDataTableForPkMySql("get")}}
+
+                    {{WriteBulkCallMySql()}}
+
+                    {{GetSelectFromTmpTable()}}
+
+                    {{WriteDapperCall()}}
+
+                    {{WriteCloseTransaction()}}
+
+                    {{WriteReturnObj()}}
+                    {{tab}}{{tab}}}
+
+                    """;
             }
-
             return string.Empty;
         }
 
         protected override string WriteDapperCall()
         {
-            var output = new StringBuilder($"{tab}{tab}{tab}var {_stringTransform.PluralizeToLower(ClassName)} = await _{_stringTransform.ApplyConfigTransformMember(_settings.DbContextClassName)}.Connection." +
-                    $"QueryAsync<{ClassName}>(sql,transaction:_{_stringTransform.ApplyConfigTransformMember(_settings.DbContextClassName)}.Transaction);");
-
-            output.Append(Environment.NewLine);
-            output.Append(Environment.NewLine);
-            output.Append($"{tab}{tab}{tab}var sqlDrop = \"DROP TABLE {ColAndTableIdentifier}tmp_bulkget_{Table.Name}{ColAndTableIdentifier};\";");
-            output.Append(Environment.NewLine);
-            output.Append($"{tab}{tab}{tab}_ = await _{_stringTransform.ApplyConfigTransformMember(_settings.DbContextClassName)}.Connection." +
-                    $"ExecuteAsync(sqlDrop,transaction:_{_stringTransform.ApplyConfigTransformMember(_settings.DbContextClassName)}.Transaction);");
-
-            return output.ToString();
+            return
+                $$"""
+                {{tab}}{{tab}}{{tab}}var {{_stringTransform.PluralizeToLower(ClassName)}} = await _{{_stringTransform.ApplyConfigTransformMember(_settings.DbContextClassName)}}.Connection.QueryAsync<{{ClassName}}>(sql,transaction:_{{_stringTransform.ApplyConfigTransformMember(_settings.DbContextClassName)}}.Transaction);
+                {{tab}}{{tab}}{{tab}}var sqlDrop = "DROP TABLE {{ColAndTableIdentifier}}tmp_bulkget_{{Table.Name}}{{ColAndTableIdentifier}};";
+                {{tab}}{{tab}}{{tab}}_ = await _{{_stringTransform.ApplyConfigTransformMember(_settings.DbContextClassName)}}.Connection.ExecuteAsync(sqlDrop,transaction:_{{_stringTransform.ApplyConfigTransformMember(_settings.DbContextClassName)}}.Transaction);
+                """;
         }
 
         protected virtual string GetSelectFromTmpTable()
         {
-            var output = new StringBuilder();
-            output.Append(WriteBaseSqlForSelect("t1."));
-            output.Append(Environment.NewLine);
-            output.Append($"{tab}{tab}{tab}INNER JOIN {ColAndTableIdentifier}tmp_bulkget_{Table.Name}{ColAndTableIdentifier} t2 ON " + Environment.NewLine +
-                $"{tab}{tab}{tab}{tab}");
-
-            //Delete fields
             var fields = String.Join($" AND ", PkColumns!.Select(c =>
                 $"t1.{ColAndTableIdentifier}{c.Name}{ColAndTableIdentifier} = t2.{ColAndTableIdentifier}{c.Name}{ColAndTableIdentifier}"));
 
-            output.Append(fields);
-            output.Append("\";");
-
-            return output.ToString();
-
+            return
+                $$""""
+                {{WriteBaseSqlForSelect("t1.")}}
+                {{tab}}{{tab}}{{tab}}INNER JOIN {{ColAndTableIdentifier}}tmp_bulkget_{{Table.Name}}{{ColAndTableIdentifier}} t2 ON 
+                {{tab}}{{tab}}{{tab}}{{tab}}{{fields}}"
+                {{tab}}{{tab}}{{tab}}""";
+                """";
         }
 
         protected override string WriteReturnObj()
