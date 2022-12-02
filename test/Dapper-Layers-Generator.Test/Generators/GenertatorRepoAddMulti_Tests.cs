52190a538300cd9dba0c721ac8d4e61a0a626192
Fabien Donnet <fabien.donnet@ubiko.ch>
2022-12-02 10:38:05 +0100

    Standard test for add multi

---
+++ test/Dapper-Layers-Generator.Test/Generators/GenertatorRepoAddMulti_Tests.cs
@@ -0,0 +1,39 @@
+﻿using Dapper_Layers_Generator.Core.Converters.MySql;
+using Dapper_Layers_Generator.Core.Converters;
+using Dapper_Layers_Generator.Core.Generators;
+using Dapper_Layers_Generator.Test.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_Layers_Generator.Test.Generators
+{
+    public class GenertatorRepoAddMulti_Tests : GeneratorBaseTest
+    {
+        private MySqlDataTypeConverter _dataConverter;
+        private StringTransformationService _transformString;
+
+        public GenertatorRepoAddMulti_Tests() : base()
+        {
+            _dataConverter = new MySqlDataTypeConverter();
+            _transformString = new StringTransformationService(_settings);
+        }
+
+        [Fact()]
+        public void GenerateStandard_Test()
+        {
+            //Arrange
+            var generator = new GeneratorRepoAddMulti(_settings, _mockDbDefinitions.Object, _transformString, _dataConverter);
+            generator.SetTable("clients");
+            var expected = ResourceTool.Read("Dapper_Layers_Generator.Test/Results/Generators/GeneratorRepoAddMulti_ResultStandard.txt");
+
+            //Act
+            var result = generator.Generate();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
