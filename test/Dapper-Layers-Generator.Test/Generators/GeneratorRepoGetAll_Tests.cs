b6bba481368b07f50410ad2a3216710ad5dbf1c4
Fabien Donnet <fabien.donnet@ubiko.ch>
2022-12-02 13:34:25 +0100

    Simple test for getall generator

---
+++ test/Dapper-Layers-Generator.Test/Generators/GeneratorRepoGetAll_Tests.cs
@@ -0,0 +1,39 @@
+﻿using Dapper_Layers_Generator.Core.Converters.MySql;
+using Dapper_Layers_Generator.Core.Converters;
+using Dapper_Layers_Generator.Core.Generators;
+using Dapper_Layers_Generator.Test.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dapper_Layers_Generator.Test.Generators
+{
+    public class GeneratorRepoGetAll_Tests : GeneratorBaseTest
+    {
+        private MySqlDataTypeConverter _dataConverter;
+        private StringTransformationService _transformString;
+
+        public GeneratorRepoGetAll_Tests() : base()
+        {
+            _dataConverter = new MySqlDataTypeConverter();
+            _transformString = new StringTransformationService(_settings);
+        }
+
+        [Fact()]
+        public void GenerateStandard_Test()
+        {
+            //Arrange
+            var generator = new GeneratorRepoGetAll(_settings, _mockDbDefinitions.Object, _transformString, _dataConverter);
+            generator.SetTable("clients");
+            var expected = ResourceTool.Read("Dapper_Layers_Generator.Test/Results/Generators/GeneratorRepoGetAll_ResultStandard.txt");
+
+            //Act
+            var result = generator.Generate();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
