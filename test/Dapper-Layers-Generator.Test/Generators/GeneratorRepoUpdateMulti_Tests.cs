ba359c4df552545e2aafb63d4dfcd759d460c7a9
Fabien Donnet <fabien.donnet@ubiko.ch>
2022-12-02 16:32:05 +0100

    Simple test for updatemuli generator

---
+++ test/Dapper-Layers-Generator.Test/Generators/GeneratorRepoUpdateMulti_Tests.cs
@@ -0,0 +1,34 @@
+﻿using Dapper_Layers_Generator.Core.Converters.MySql;
+using Dapper_Layers_Generator.Core.Converters;
+using Dapper_Layers_Generator.Core.Generators;
+using Dapper_Layers_Generator.Test.Config;
+
+namespace Dapper_Layers_Generator.Test.Generators
+{
+    public class GeneratorRepoUpdateMulti_Tests : GeneratorBaseTest
+    {
+        private MySqlDataTypeConverter _dataConverter;
+        private StringTransformationService _transformString;
+
+        public GeneratorRepoUpdateMulti_Tests() : base()
+        {
+            _dataConverter = new MySqlDataTypeConverter();
+            _transformString = new StringTransformationService(_settings);
+        }
+
+        [Fact()]
+        public void GenerateStandard_Test()
+        {
+            //Arrange
+            var generator = new GeneratorRepoUpdateMulti(_settings, _mockDbDefinitions.Object, _transformString, _dataConverter);
+            generator.SetTable("clients");
+            var expected = ResourceTool.Read("Dapper_Layers_Generator.Test/Results/Generators/GeneratorRepoUpdateMulti_ResultStandard.txt");
+
+            //Act
+            var result = generator.Generate();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
