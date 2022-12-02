01f81be9b8531a1af7ed1edf8407e38017943993
Fabien Donnet <fabien.donnet@ubiko.ch>
2022-12-02 11:53:14 +0100

    DeleteBulkRepo simple test standard and composite PK for MySql

---
+++ test/Dapper-Layers-Generator.Test/Generators/MySql/MySqlGenertatorRepoDeleteBulk_Tests.cs
@@ -0,0 +1,55 @@
+﻿using Dapper_Layers_Generator.Core.Converters.MySql;
+using Dapper_Layers_Generator.Core.Converters;
+using Dapper_Layers_Generator.Core.Generators;
+using Dapper_Layers_Generator.Test.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper_Layers_Generator.Core.Generators.MySql;
+
+namespace Dapper_Layers_Generator.Test.Generators.MySql
+{
+    public class MySqlGenertatorRepoDeleteBulk_Tests : GeneratorBaseTest
+    {
+        private MySqlDataTypeConverter _dataConverter;
+        private StringTransformationService _transformString;
+
+        public MySqlGenertatorRepoDeleteBulk_Tests() : base()
+        {
+            _dataConverter = new MySqlDataTypeConverter();
+            _transformString = new StringTransformationService(_settings);
+        }
+
+        [Fact()]
+        public void GenerateStandard_Test()
+        {
+            //Arrange
+            var generator = new MySqlGeneratorRepoDeleteBulk(_settings, _mockDbDefinitions.Object, _transformString, _dataConverter);
+            generator.SetTable("clients");
+            var expected = ResourceTool.Read("Dapper_Layers_Generator.Test/Results/Generators/MySql/MySqlGeneratorRepoDeleteBulk_ResultStandard.txt");
+
+            //Act
+            var result = generator.Generate();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact()]
+        public void GenerateCompositePk_Test()
+        {
+            //Arrange
+            var generator = new MySqlGeneratorRepoDeleteBulk(_settings, _mockDbDefinitions.Object, _transformString, _dataConverter);
+            generator.SetTable("damages_failures");
+            var expected = ResourceTool.Read("Dapper_Layers_Generator.Test/Results/Generators/MySql/MySqlGeneratorRepoDeleteBulk_ResultCompositePk.txt");
+
+            //Act
+            var result = generator.Generate();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
