ed6a31fff34960f3b9b6fa39b35d5fc97c41cb4d
Fabien Donnet <fabien.donnet@ubiko.ch>
2022-12-02 16:21:00 +0100

    Simple test for MySqlUpdate bulk generator

---
+++ test/Dapper-Layers-Generator.Test/Generators/MySql/MySqlGeneratorRepoUpdateBulk_Tests.cs
@@ -0,0 +1,34 @@
+﻿using Dapper_Layers_Generator.Core.Converters.MySql;
+using Dapper_Layers_Generator.Core.Converters;
+using Dapper_Layers_Generator.Core.Generators.MySql;
+using Dapper_Layers_Generator.Test.Config;
+
+namespace Dapper_Layers_Generator.Test.Generators.MySql
+{
+    public class MySqlGeneratorRepoUpdateBulk_Tests : GeneratorBaseTest
+    {
+        private MySqlDataTypeConverter _dataConverter;
+        private StringTransformationService _transformString;
+
+        public MySqlGeneratorRepoUpdateBulk_Tests() : base()
+        {
+            _dataConverter = new MySqlDataTypeConverter();
+            _transformString = new StringTransformationService(_settings);
+        }
+
+        [Fact()]
+        public void GenerateStandard_Test()
+        {
+            //Arrange
+            var generator = new MySqlGeneratorRepoUpdateBulk(_settings, _mockDbDefinitions.Object, _transformString, _dataConverter);
+            generator.SetTable("clients");
+            var expected = ResourceTool.Read("Dapper_Layers_Generator.Test/Results/Generators/MySql/MySqlGeneratorRepoUpdateBulk_ResultStandard.txt");
+
+            //Act
+            var result = generator.Generate();
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
